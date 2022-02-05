02961630e50c326bf0f1361068320906e201f740
nicolepage <npage1129@gmail.com>
2022-02-04 22:31:36 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function  writeCards(names, event){
+  let newArray = [];
+for (let i = 0; i < names.length; i++){; 
+    let message = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`); 
+   newArray.push(message);
+}
+return newArray;
+}
+
+function countDown(integer){
+while(integer >= 0){
+    console.log(integer);
+    integer--;
+}
+}
